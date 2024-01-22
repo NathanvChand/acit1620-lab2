6c6811c2248705d08864c96ddde2439e2b203b8f
Nathan Chand <nathanchand2004@gmail.com>
2024-01-21 22:11:50 -0800

    Lab2 ACIT1620

---
+++ Lesson2Python.py
@@ -0,0 +1,54 @@
+print('Testing')
+
+"""
+Steps for running .py files
+
+1. Create your file in Vscode, save as with .py extension
+2. Right-click on the tab for that file, select that copy path
+3.Paste the path into the terminal (inside of quote)
+4. cd 'C:\Users\natha\OneDrive\Desktop\acit1620\lab 2>'
+5. Run the file with py/python/python3 file name
+
+Note :
+If you're using
+
+"""
+
+"""
+Operators
+=   assignment
+
+
+
+
+
+
+"""
+
+"""
++ addition / joining strings
+-
+
++=
+-+
+*=
+/=
+
+"""
+Comparison Operators
+
+"""
+
+"""
+
+counter = 0 # creating a variable called counter, assigning 0
+print (counter)
+
+# counter = counter + 1   # takw the current value of counter, add 1
+# print(counter)
+
+counter += 1 # equivalent to counter = counter + 1
+counter *= 2 # quivalent to counter = counter * 2
+print(counter)
+
+print(counter == 2) # prints True
